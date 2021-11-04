a372b13952f303dcc1949eab20fcadd8ffa661fd
Vinícius Santiago <viniciussdsilva@gmail.com>
2021-11-04 18:13:43 -0300

    refactor: remove components

--- src/components/I18n/index.ts
+++
@@ -1,4 +0,0 @@
-import { LanguageSelector } from './LanguageSelector';
-import { Translator } from './Translator';
-
-export { LanguageSelector, Translator };
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,4 @@
-import { LanguageSelector, Translator } from './I18n';
 import { Loader } from './Loader';
-import { Logo } from './Logo';
 import { PageTitle } from './PageTitle';
 
-export { LanguageSelector, Logo, Loader, PageTitle, Translator };
+export { Loader, PageTitle };
