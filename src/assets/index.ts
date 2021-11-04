0d9fc6d73ed2812f60f292229b9dcb65d40de895
Vinícius Santiago <viniciussdsilva@gmail.com>
2021-11-04 18:13:04 -0300

    refactor: remove assets

--- src/assets/index.ts
+++
@@ -1,6 +0,0 @@
-import Dietitian from './dietitian.svg';
-import DietitianD from './dietitian-d.svg';
-import Nutricionista from './nutricionista.svg';
-import NutricionistaN from './nutricionista-n.svg';
-
-export { Dietitian, DietitianD, Nutricionista, NutricionistaN };
